0249b6fb235fb599464e8620f790c3643c91d7a4
BIENsiete <biensiete@gmail.com>
2026-04-28 15:39:38 -0300

    Fix: Gradle 9.0 with JDK 25, no toolchain, target Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,8 @@ dependencies {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile> {
